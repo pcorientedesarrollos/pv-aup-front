7bf00ce58aa2d1d30771500e2d220ae4f6d81299
Emmanuel Manzanilla <emazanillabaas@gmail.com>
2026-09-15 11:25:55 -0600

    resolve

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-const fs = require('fs'); fs.appendFileSync('src/app/features/corte-caja/corte-caja.component.html', \\n\n\\);
+﻿const fs = require('fs');
+let content = fs.readFileSync('src/app/features/productos/productos.component.html', 'utf8');
+const replaceBlock = fs.readFileSync('replacement.html', 'utf8');
+
+const regex = /<div class="flex gap-3 flex-wrap">[\s\S]*?Ganancia Proyectada[\s\S]*?<\/div>\s*<\/div>\s*<\/div>/m;
+content = content.replace(regex, replaceBlock);
+fs.writeFileSync('src/app/features/productos/productos.component.html', content, 'utf8');
